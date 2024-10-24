5af1aa0869fd1f55704683636acb7c5a03846f3b
gsygh <gsygh@naver.com>
2024-10-24 23:58:03 +0900

    create CShop Project(exception Ignore)

---
+++ test/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+// C#은 문자열을 ''로 묶지 말 것
+Console.WriteLine("Hello, World!");
+Console.WriteLine("The current time is " + DateTime.Now);
+
+Console.Write("Congratulations!");
+Console.Write(" ");
+Console.Write("You wrote your first lines of code.");
